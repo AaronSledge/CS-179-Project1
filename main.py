aae28cf56bc2fea8650e36fc051d824263a7c7fd
AaronSledge <ahern2118@gmail.com>
2025-10-22 16:05:32 -0700

    Create baseline algorithim(not done)

--- main.py
+++ main.py
@@ -3,7 +3,7 @@
 #inputs: file with N locations, Enter key interpution(char or askii value)
 #Outputs: Sum of distance(int), paths of points(array), if solution is greater than 6000...(string), any error messaging(string)
 import math
-
+from randomS import randomSearch
 class Location:
     def __init__(self, number, x, y):
         self.number = number
@@ -31,19 +31,27 @@ def FileRead(filename):
     file.close()
     return listOfPoints
 
-def Euclidean(x1, y1, x2, y2):
-    distance = math.sqrt(math.pow((x2 - x1), 2) + math.pow((y2 - x1), 2))
-    return distance
-
 filename = input("Enter the name of file: ")
 listOfPoints = FileRead(filename)
-distanceMatrix = []
-for i in range(len(listOfPoints)):
-    row = []
-    for j in range(len(listOfPoints)):
-        row.append((Euclidean(listOfPoints[i].x, listOfPoints[i].y, listOfPoints[j].x, listOfPoints[j].x), listOfPoints[i], listOfPoints[j]))
-    distanceMatrix.append(row)
 
-print(len(distanceMatrix))
+sumOfDistance = math.inf
+
+print(f"There are {(len(listOfPoints))}, computing route...")
+print("\t Shortest Route Discovered So Far")
+
+while True:
+    stopPoint = input()
+    if stopPoint == "s":
+        break
+    sumOfDistance, path = randomSearch(listOfPoints, sumOfDistance)
+    print(sumOfDistance)
+
+
+
+
+
+
+
+
 
 
--- randomS.py
+++ randomS.py
@@ -2,5 +2,19 @@
 #Inputs: Memo table(euclidean distance), List of points(array), Sum of distance(float)
 #Returns: Sum of distance, path shown
 #Aaron
-def randomSearch(distances, listOfPoints, sumOfDistance):
-    return 0
+import random
+from euclideanDistance import Euclidean
+
+def randomSearch(listOfPoints, sumOfDistance):
+    middle = listOfPoints[1:len(listOfPoints)-2]
+    randomList = random.sample(middle, len(middle))
+
+    path = [listOfPoints[0]] + randomList + [listOfPoints[-1]]
+    distance = 0
+    for i in range(len(path) - 1):
+        distance += Euclidean(path[i].x, path[i].y, path[i + 1].x, path[i + 1].y)
+        #print(path[i].x, path[i].y, path[i + 1].x, path[i + 1].y)
+    
+    if(distance < sumOfDistance):
+        return distance,path
+    return sumOfDistance,path
