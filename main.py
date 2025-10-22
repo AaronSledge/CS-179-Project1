afeb3e2db285f3cacc049df64c717ba5b9002096
Kenneth Ea <kennethea@Kenneths-MacBook-Pro-3.local>
2025-10-21 20:43:55 -0700

    Adding main

--- main.py
+++ main.py
@@ -2,3 +2,45 @@
 #Kenny/michael
 #inputs: file with N locations, Enter key interpution(char or askii value)
 #Outputs: Sum of distance(int), paths of points(array), if solution is greater than 6000...(string), any error messaging(string)
+import math
+
+class Location:
+    def __init__(self, number, x, y):
+        self.number = number
+        self.x = x
+        self.y = y
+
+def FileRead(filename):
+    file = open(filename)
+
+    array = []
+    number = 0
+
+    while True:
+        line = file.readline()
+        if(line == ""):
+            break
+        else:
+            x, y = line.split()
+            x = float(x)
+            y = float(y)
+            number = number + 1
+            node = Location(number, x, y)
+            array.append(node)
+        
+    file.close()
+    return array
+
+def Euclidean(x1, y1, x2, y2):
+    distance = math.sqrt(math.pow((x2 - x1), 2) + math.pow((y2 - x1), 2))
+    return distance
+
+filename = input("Enter the name of file: ")
+array = FileRead(filename)
+
+d = Euclidean(array[1].x, array[1].y, array[2].x, array[2].x)
+
+print(d)
+
+
+
