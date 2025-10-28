5b05b9e1597810b9d6231943ed748d95e58f3081
Kenneth Ea <kennethea@Kenneths-MacBook-Pro-3.local>
2025-10-27 20:06:33 -0700

    Adding output file

--- main.py
+++ main.py
@@ -5,10 +5,10 @@
 import math
 import DistanceMatrix
 from euclideanDistance import Euclidean
-import randomNN
-import ClassicNN
-import ModifiedNN
-import EarlyAbandoning
+#import randomNN
+#import ClassicNN
+#import ModifiedNN
+#import EarlyAbandoning
 from randomS import randomSearch
 import threading
 import time
@@ -47,28 +47,35 @@ finalPath = []
 def printSum(sumOfDistance, listOfPoints):
     global collectionOfDistance, finalPath #so variables are mutable within thread and function
     start_time = time.time()
+    i = 1
     while not isDone:
-        time.sleep(0.5) #code pauses half a second. Can change if needed to
+        time.sleep(0.25) #code pauses half a second. Can change if needed to
         sumOfDistance, path = randomSearch(listOfPoints, sumOfDistance)
         time_So_Far = time.time() - start_time 
         collectionOfDistance.append((sumOfDistance, time_So_Far)) #for jason when making distance over time graph
-        finalPath = path
-        print(f"\t \t {sumOfDistance}")
-
+        if i == 1:
+            finalPath = path
+            print(f"\t \t {sumOfDistance}")
+        else:
+            if sumOfDistance < collectionOfDistance[-2][0]:
+                finalPath = path
+                print(f"\t \t {sumOfDistance}")
+        i += i 
 
 filename = input("Enter the name of file: ")
 listOfPoints = FileRead(filename)
 
-
-
-print(f"There are {(len(listOfPoints))}, computing route...")
+print(f"There are {(len(listOfPoints))} nodes, computing route...")
 print("\t Shortest Route Discovered So Far")
 
 threading.Thread(target=printSum, args=(math.inf, listOfPoints)).start() #used threading so function can continously run without having to wait for input
 
-
 input()
 isDone = True
+
+with open(f"{filename}_SOLUTION_{collectionOfDistance[-1][0]}", "w") as outFile:
+    for i in finalPath:
+        outFile.write(f"{i.number} \n")
 
 #print(collectionOfDistance) uncomment this to see array of distance and time(in seconds)assoicated with
 
@@ -78,7 +85,7 @@ isDone = True
 
 
 
-length = len(array)
+#length = len(array)
 # for i in range(length):
 #     if i == 0:
 #         print(f'Length of array: {length}')
@@ -88,8 +95,8 @@ length = len(array)
 
 
 # calculate distance matrix here, but how do we do make this matrix?
-dist_mat = DistanceMatrix.dist_matrix(array)
-print(dist_mat)
+#dist_mat = DistanceMatrix.dist_matrix(array)
+#print(dist_mat)
 
 # pass dist matrix as a parameter to the RandomNN function?
 #solution = randomNN(dist_mat, starting_alg=ClassicNN, second_alg=ModifiedNN, calculate_dist=Euclidean, optimizer=EarlyAbandoning)
