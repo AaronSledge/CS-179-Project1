6e0bcc135fe210650d8645b10753040641460888
Kenneth Ea <kennethea@Kenneths-MacBook-Pro-3.local>
2025-10-27 20:09:17 -0700

    Updated Euclidean file

--- euclideanDistance.py
+++ euclideanDistance.py
@@ -5,9 +5,6 @@
 import math
 
 def Euclidean(x1, y1, x2, y2):
-    distance = math.sqrt(math.pow((x2 - x1), 2) + math.pow((y2 - x1), 2))
-    return distance
-def Euclidean(x1, y1, x2, y2):
     distance = math.sqrt(math.pow((x2 - x1), 2) + math.pow((y2 - y1), 2))
     return distance
 
